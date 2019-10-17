026c786b4b078ae5fb6c3ef4adce98556a0c87de
Ahmed <43687458+AhmedZaki918@users.noreply.github.com>
2019-10-17 16:36:47 +0200

    Added Java & Android library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,8 +8,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.android.androidlibrary.DisplayJoke;
+import com.example.android.javalibrary.JavaJoke;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +45,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        JavaJoke javaLibrary = new JavaJoke();
+
+        Intent intent = new Intent(MainActivity.this, DisplayJoke.class);
+        intent.putExtra("key", javaLibrary.getJoke());
+        startActivity(intent);
+
     }
-
-
 }
---
+++ javaLibrary/src/main/java/com/example/android/javalibrary/JavaJoke.java
@@ -0,0 +1,11 @@
+package com.example.android.javalibrary;
+
+public class JavaJoke {
+
+
+    private static final String JOKE = "funny joke";
+
+    public String getJoke() {
+        return JOKE;
+    }
+}
